f05ac5e3837942deb4849f4c5e5ec60e3f920ca2
Usama Rahman <95021960+usama-rahman@users.noreply.github.com>
2024-02-26 12:12:03 +0600

    OK

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,13 +20,57 @@ function Hero() {
                 </span>
               </a>
             </div>
-            <h1></h1>
-            <div></div>
-            <div></div>
-            <div></div>
+            <h1 className="line text-3xl font-[800]	 leading-tight text-[#292940] md:text-4xl ">
+              <span className="md:text-[2.5rem]">
+                <font>Easily convert your website visitors into</font>
+              </span>
+
+              <font>
+                <span className="opacity-100 ">|</span>
+              </font>
+              <span className=""></span>
+            </h1>
+
+            <div className="mt-4 text-lg md:mt-6 md:pr-8  ">
+              <p className="text-[#575778]">
+                <font>With Futy&apos;s </font>
+                <strong className="font-[700] ">Leadbots</strong>
+                <font>
+                  , your website converts visitors into Leads in a
+                  customer-friendly manner, including via WhatsApp, Video call
+                  or a Quote Request.
+                </font>
+              </p>
+            </div>
+
+            <div className="mb-0 mt-8 flex flex-wrap items-center justify-start  ">
+              <button className="relative mb-2 mr-2 hidden h-[2.857em] cursor-pointer items-center justify-center text-nowrap rounded-[4px] border border-[#5f77ff] bg-white px-[1.125em] py-1.5 text-center align-top	 font-semibold	leading-normal text-[#5f77ff] hover:bg-[#5f77ff] hover:text-white md:inline-flex ">
+                Schedule a demo
+              </button>
+
+              <a className="relative mb-2 mr-2 inline-flex h-[2.857em] cursor-pointer items-center justify-center text-nowrap rounded-[4px] border border-[#5f77ff] bg-[#5f77ff] px-[1.125em] py-1.5 text-center align-top	 	font-semibold leading-normal text-white hover:bg-[#566add] hover:text-white ">
+                create an account
+              </a>
+            </div>
+
+            <div className="mt-3 text-sm text-[#7f7f8c] ">
+              <p>
+                <em>
+                  <font>Installed on </font>
+                  <strong>any</strong>
+                  <font> website within 5 minutes.</font>
+                </em>
+              </p>
+            </div>
           </div>
           {/* Image */}
-          <div className=" md:min-[550px] min-[550px] flex flex-1 items-center justify-center md:justify-end "></div>
+          <div className=" md:min-[550px] min-[550px] flex flex-1 items-center justify-center md:justify-end ">
+            <img
+              src="https://images.prismic.io/futy-io/c8d56270-9fbc-43fd-ba88-4ded57b8bc7d_hero-homepage.jpg?auto=compress,format&rect=0,0,1100,1100&w=550"
+              alt="Futy"
+              className="block h-auto max-h-[550px] w-full max-w-[550px]  "
+            />
+          </div>
         </div>
       </div>
     </section>
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,9 +1,4 @@
-/* eslint-disable react/prop-types */
-const Button = ({ text }) => {
-  return (
-    <button className="rounded-md bg-[#5f77ff] px-3 py-2 text-[14px] font-semibold text-white ">
-      {text}
-    </button>
-  );
-};
+function Button() {
+  return <div>Button</div>;
+}
 export default Button;
