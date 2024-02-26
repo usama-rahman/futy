f3f57be408421263c1a7b32e131634ce00df9916
Usama Rahman <95021960+usama-rahman@users.noreply.github.com>
2024-02-27 00:14:20 +0600

    Done

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -33,26 +33,21 @@ const data = [
 ];
 
 function Slide() {
-  const [imgSlide, setImgSlide] = useState(data);
-  const [index, setIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-  console.log(index);
-  useEffect(() => {
-    const lastIndex = imgSlide.length - 1;
-    if (index < 0) {
-      setIndex(lastIndex);
-    }
-    if (index > lastIndex) {
-      setIndex(0);
-    }
-  }, [index, imgSlide]);
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newSlide = isFirstSlide ? data.length - 1 : currentIndex - 1;
 
-  useEffect(() => {
-    let slider = setInterval(() => {
-      setIndex(index + 1);
-    }, 3000);
-    return () => clearInterval(slider);
-  }, [index]);
+    setCurrentIndex(newSlide);
+  };
+
+  const nextSlide = () => {
+    const isFirstSlide = currentIndex === data.length - 1;
+    const newSlide = isFirstSlide ? 0 : currentIndex + 1;
+
+    setCurrentIndex(newSlide);
+  };
 
   return (
     <section className=" block px-6 py-12 md:px-6 md:py-16 ">
@@ -60,69 +55,88 @@ function Slide() {
         <div className="px-12 md:py-0  ">
           <div>
             <div
-              onClick={() => setIndex(index - 1)}
+              onClick={prevSlide}
               className=" absolute left-0 top-1/2 z-10 flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-[#eff1ff] p-0 leading-[0] text-[#5f77ff] "
             >
               <img src={LeftSlide} alt="Left" />
             </div>
 
             <div className="relative m-0 block overflow-hidden p-0  ">
-              {imgSlide.map((item, imgIndex) => {
-                let position = 'nextSlide';
-
-                if (imgIndex === index) {
-                  position = 'activeSlide';
-                }
-                if (
-                  imgIndex === index - 1 ||
-                  (index === 0 && imgIndex === imgSlide.length - 1)
-                ) {
-                  position = 'lastSlide';
-                }
-
-                return (
-                  <div key={item.id} className="relative left-0 top-0 block ">
-                    <img src={item.image} alt={item.id} />
-                  </div>
-                );
-              })}
+              <div className="relative left-0 top-0 block ">
+                <img src={data[currentIndex].image} alt="" />
+              </div>
             </div>
 
             <div
-              onClick={() => setIndex(index + 1)}
+              onClick={nextSlide}
               className=" absolute right-0 top-1/2 z-10 flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-[#eff1ff] p-0 leading-[0] text-[#5f77ff]"
             >
               <img src={RightSlide} alt="Right" />
             </div>
           </div>
           <ul className=" absolute bottom-0 left-1/2 m-0 mt-2 flex w-full  -translate-x-1/2 transform items-center justify-center p-0 ">
-            <li className="relative mx-1 my-0 inline-block h-5 w-5 cursor-pointer p-0 ">
+            <li
+              onClick={() => setCurrentIndex(0)}
+              className="relative mx-1 my-0 inline-block h-5 w-5 cursor-pointer p-0 "
+            >
               <div className="flex h-full items-center justify-center ">
-                <span className=" h-1.5 w-1.5 rounded-3xl bg-[#5f77ff] opacity-100 "></span>
+                <span
+                  className={`h-1.5 w-1.5 rounded-3xl bg-[#5f77ff] ${
+                    currentIndex === 0 ? 'opacity-100' : 'opacity-50'
+                  }`}
+                ></span>
               </div>
             </li>
 
-            <li className="relative mx-1 my-0 inline-block h-5 w-5 cursor-pointer p-0 ">
+            <li
+              onClick={() => setCurrentIndex(1)}
+              className="relative mx-1 my-0 inline-block h-5 w-5 cursor-pointer p-0 "
+            >
               <div className="flex h-full items-center justify-center ">
-                <span className=" h-1.5 w-1.5 rounded-3xl bg-[#5f77ff] opacity-50 "></span>
+                <span
+                  className={`h-1.5 w-1.5 rounded-3xl bg-[#5f77ff] ${
+                    currentIndex === 1 ? 'opacity-100' : 'opacity-50'
+                  }`}
+                ></span>
               </div>
             </li>
 
-            <li className="relative mx-1 my-0 inline-block h-5 w-5 cursor-pointer p-0 ">
+            <li
+              onClick={() => setCurrentIndex(2)}
+              className="relative mx-1 my-0 inline-block h-5 w-5 cursor-pointer p-0 "
+            >
               <div className="flex h-full items-center justify-center ">
-                <span className=" h-1.5 w-1.5 rounded-3xl bg-[#5f77ff] opacity-50 "></span>
+                <span
+                  className={`h-1.5 w-1.5 rounded-3xl bg-[#5f77ff] ${
+                    currentIndex === 2 ? 'opacity-100' : 'opacity-50'
+                  }`}
+                ></span>
               </div>
             </li>
 
-            <li className="relative mx-1 my-0 inline-block h-5 w-5 cursor-pointer p-0 ">
+            <li
+              onClick={() => setCurrentIndex(3)}
+              className="relative mx-1 my-0 inline-block h-5 w-5 cursor-pointer p-0 "
+            >
               <div className="flex h-full items-center justify-center ">
-                <span className=" h-1.5 w-1.5 rounded-3xl bg-[#5f77ff] opacity-50 "></span>
+                <span
+                  className={`h-1.5 w-1.5 rounded-3xl bg-[#5f77ff] ${
+                    currentIndex === 3 ? 'opacity-100' : 'opacity-50'
+                  }`}
+                ></span>
               </div>
             </li>
 
-            <li className="relative mx-1 my-0 inline-block h-5 w-5 cursor-pointer p-0 ">
+            <li
+              onClick={() => setCurrentIndex(4)}
+              className="relative mx-1 my-0 inline-block h-5 w-5 cursor-pointer p-0 "
+            >
               <div className="flex h-full items-center justify-center ">
-                <span className=" h-1.5 w-1.5 rounded-3xl bg-[#5f77ff] opacity-50 "></span>
+                <span
+                  className={`h-1.5 w-1.5 rounded-3xl bg-[#5f77ff] ${
+                    currentIndex === 4 ? 'opacity-100' : 'opacity-50'
+                  }`}
+                ></span>
               </div>
             </li>
           </ul>
